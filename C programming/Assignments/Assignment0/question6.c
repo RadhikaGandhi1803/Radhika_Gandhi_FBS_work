5ebd1153f99223a9aea926cf183c19a441d51c27
RadhikaGandhi1803 <gandhiradhika176@gmail.com>
2025-06-01 21:14:20 +0530

    Add files via upload

---
+++ C programming/Assignments/Assignment0/question6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int num=15;
+	int square,cube;
+	
+	square=num*num;
+	cube=num*num*num;
+	
+	printf("The square of %d is:%d\n",num,square);
+	printf("The cube of %d is:%d\n",num,cube);
+
+}
