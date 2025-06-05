8f1903344158a90065db9b85021c775af66d64a7
RadhikaGandhi1803 <gandhiradhika176@gmail.com>
2025-06-05 12:25:14 +0530

    Delete C programming/Assignments directory

--- C programming/Assignments/Assignment0/Question2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	float radius=5.0;
-	double area;
-	area= 3.14*radius*radius;
-	
-	printf("The area of cirle with radius %2.f is :%lf",radius,area);
-}
--- C programming/Assignments/Assignment0/Question3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	float celsius=35.0;
-	float fahrenheit;
-	fahrenheit=(celsius *9/5)+32;
-	printf("%f celsius = %f fahrenheit", celsius,fahrenheit);
-}
