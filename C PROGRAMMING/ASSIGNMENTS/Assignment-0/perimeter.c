e2ba4e0e564f4062c7881ef750632716165cbe72
RadhikaGandhi1803 <gandhiradhika176@gmail.com>
2025-06-05 23:37:19 +0530

    Add files via upload

---
+++ C PROGRAMMING/ASSIGNMENTS/Assignment-0/perimeter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	float lenght=5.0;
+	float width=4.8;
+	float perimeter;
+	
+	perimeter=2*(lenght+width);
+	
+	printf("Perimeter of rectangle is %f",perimeter);
+}
